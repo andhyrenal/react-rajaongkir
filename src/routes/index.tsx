30989ff1c1f7100c289cc1e8e771b11b725841f8
Andhy Renal <andhy@radyalabs.id>
2025-03-15 09:40:23 +0700

    fix: back to browser router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/", element: <Home /> },
 ]);
